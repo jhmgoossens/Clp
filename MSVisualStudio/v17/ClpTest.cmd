df26bccbcfb9d08c6ba7c4fb3cef081f4fa5543b
Jan-Willem Goossens <jhmgoossens@hotmail.com>
2023-03-18 09:17:57 +0100

    Improve unit testing for MSVS build

--- MSVisualStudio/v17/ClpTest.cmd
+++ MSVisualStudio/v17/ClpTest.cmd
@@ -3,23 +3,32 @@
 @echo off
 SET "BINDIR=%~1"
 SET "SAMPLEDIR=%~2"
+SET "NETLIBDIR=%~3"
+SET "MIPLIBDIR=%~4"
 
 echo INFO: Running %0 %*
-echo INFO: Using bindir '%BINDIR%' and sampledir '%SAMPLEDIR%'
+echo INFO: Using bindir '%BINDIR%' and sampledir '%SAMPLEDIR%' and netlibdir '%NETLIBDIR%'
+echo INFO: Miplibdir '%MIPLIBDIR%' is ignored for these tests.
 
-if "%BINDIR%"=="" goto :usage
-if not exist "%BINDIR%" echo ERROR: Folder %BINDIR% does not exist. && goto :usage
-if "%SAMPLEDIR%"=="" goto :usage
-if not exist %SAMPLEDIR% echo ERROR: Folder %SAMPLEDIR% does not exist. && goto :usage
+if "%BINDIR%"=="" echo ERROR: No bindir given. && goto :usage
+if not exist "%BINDIR%" echo ERROR: Folder bindir %BINDIR% does not exist. && goto :usage
+
+if "%SAMPLEDIR%"=="" echo ERROR: No sampledir given. && goto :usage
+if not exist %SAMPLEDIR% echo ERROR: Folder sampledir %SAMPLEDIR% does not exist. && goto :usage
 if not %errorlevel%==0 echo ERROR: %SAMPLEDIR% cannot contain spaces. && goto :usage
+
+if "%NETLIBDIR%"=="" echo ERROR: No netlibdir given. && goto :usage
+if not exist %NETLIBDIR% echo ERROR: Folder netlibdir %NETLIBDIR% does not exist. && goto :usage
+if not %errorlevel%==0 echo ERROR: %NETLIBDIR% cannot contain spaces. && goto :usage
 
 goto :test
 
 :usage
-echo INFO: Usage %0 ^<bindir^> ^<sampledir^>
-echo INFO: where ^<bindir^> contains the executables and sampledir the sample files.
-echo INFO: The ^<sampledir^> must not contain spaces!
-echo INFO: For example: %0 "D:\Some Directory\" "..\..\samples\"
+echo INFO: Usage %0 ^<bindir^> ^<sampledir^> ^<netlibdir^>
+echo INFO: where ^<bindir^> contains the executables, sampledir the sample files and netlibdir the netlib files.
+echo INFO: This script runs automated test.
+echo INFO: The ^<sampledir^> and ^<netlibdir^> must not contain spaces!
+echo INFO: For example: %0 "D:\Some Directory\" ..\..\samples\ "..\..\netlib"
 goto :error
 
 :error
@@ -30,12 +39,10 @@ exit /b 1
 :test
 echo INFO: Starting Tests
 
-"%BINDIR%\clp.exe" -dirSample %SAMPLEDIR% -unitTest
-if not %errorlevel%==0 goto :error
+"%BINDIR%\clp.exe" -dirSample %SAMPLEDIR% -unitTest -dirNetlib %netlibdir% -netlib
+if not %errorlevel%==0 echo ERROR: Error running clp.exe tests. && goto :error
 
-if exist "%BINDIR%\OsiClpUnitTest.exe" (
-  "%BINDIR%\OsiClpUnitTest.exe" -mpsDir=%SAMPLEDIR%
-  if not %errorlevel%==0 goto :error
-)
+"%BINDIR%\osiUnitTest.exe" -mpsDir=%SAMPLEDIR% -netlibDir=%NETLIBDIR% -testOsiSolverInterface 
+if not %errorlevel%==0 echo ERROR: Error running osiUnitTest.exe tests. && goto :error
 
 echo INFO: Finished Tests successfully (%ERRORLEVEL%)
